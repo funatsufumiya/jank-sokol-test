c126b34f1b63141d8669948b737b15d7e25e0955
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-03-28 13:48:14 +0900

    fix: not include sokol_app.h

--- cpp_src/app.cpp
+++ cpp_src/app.cpp
@@ -5,6 +5,7 @@
 #define SOKOL_DLL
 #define SOKOL_NO_ENTRY
 #define SOKOL_GLCORE
+#include <sokol_log.h>
 #include <sokol_app.h>
 
 #if defined(_WIN32)
@@ -15,8 +16,20 @@
 
 extern "C" {
 
-void APPAPI run_app(sapp_desc* desc){
-  sapp_run(*desc);
+void APPAPI run_app(
+    int width, int height,
+    void (*init_cb)(void),
+    void (*frame_cb)(void),
+    void (*cleanup_cb)(void)
+){
+  sapp_desc app_desc = {};
+  app_desc.width = width;
+  app_desc.height = height;
+  app_desc.init_cb = init_cb;
+  app_desc.frame_cb = frame_cb;
+  app_desc.cleanup_cb = cleanup_cb;
+  app_desc.logger.func = slog_func;
+  sapp_run(app_desc);
 }
 
-}
+} // extern "C"
